f975102642252a42e6ec51956b76c6ca10e05214
Teerapong Singthong <teerapong.singthong@krungthai-axa.co.th>
2018-05-03 17:03:45 +0700

    Add servic end point

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
-const express = require("express");
-const Prometheus = require("prom-client");
+const express = require('express');
+const Prometheus = require('prom-client');
 
 const app = express();
+
+// matrix
 const metricsInterval = Prometheus.collectDefaultMetrics();
+
+// validation matrix
+const validationTotal = new Prometheus.Counter({
+  name: 'validations_total',
+  help: 'Total number of validate',
+  labelNames: ['etl_validation']
+});
+
 const checkoutsTotal = new Prometheus.Counter({
-  name: "checkouts_total",
-  help: "Total number of checkouts",
-  labelNames: ["payment_method"]
+  name: 'checkouts_total',
+  help: 'Total number of checkouts',
+  labelNames: ['payment_method']
 });
 const httpRequestDurationMicroseconds = new Prometheus.Histogram({
-  name: "http_request_duration_ms",
-  help: "Duration of HTTP requests in ms",
-  labelNames: ["method", "route", "code"],
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
   buckets: [0.1, 5, 15, 50, 100, 200, 300, 400, 500] // buckets for response time from 0.1ms to 500ms
 });
 
@@ -21,30 +31,74 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res, next) => {
+app.get('/', (req, res, next) => {
   setTimeout(() => {
-    res.json({ message: "Hello World!" });
+    res.json({ message: 'Hello World!' });
     next();
   }, Math.round(Math.random() * 200));
 });
 
-app.get("/bad", (req, res, next) => {
-  next(new Error("My Error"));
+// kaldis submitted
+const submitCounter = new Prometheus.Counter({
+  name: 'submit_policy',
+  help: 'Total submit policy',
+  labelNames: ['submit_policy']
+});
+app.get('/submit', (req, res, next) => {
+  submitCounter.inc({
+    success: Math.round(Math.random()) === 0 ? false : true
+  });
+
+  res.json(submitCounter);
+
+  next();
 });
 
-app.get("/checkout", (req, res, next) => {
-  const paymentMethod = Math.round(Math.random()) === 0 ? "stripe" : "paypal";
+const verifyCounter = new Prometheus.Counter({
+  name: 'verify_policy',
+  help: 'Total verify policy',
+  labelNames: ['verify_policy']
+});
+app.get('/verify', (req, res, next) => {
+  verifyCounter.inc({
+    success: Math.round(Math.random()) === 0 ? false : true
+  });
+
+  res.json(verifyCounter);
+
+  next();
+});
+
+app.get('/bad', (req, res, next) => {
+  next(new Error('My Error'));
+});
+
+app.get('/validate', (req, res, next) => {
+  const validationResult =
+    Math.round(Math.random()) === 0 ? 'passed' : 'failed';
+
+  validationTotal.inc({
+    etl_validation: validationResult
+  });
+
+  res.json({ status: `validate ${validationResult}` });
+  next();
+});
+
+
+app.get('/checkout', (req, res, next) => {
+  const paymentMethod = Math.round(Math.random()) === 0 ? 'stripe' : 'paypal';
 
   checkoutsTotal.inc({
     payment_method: paymentMethod
   });
 
-  res.json({ status: "ok" });
+  res.json({ status: 'ok' });
   next();
 });
 
-app.get("/metrics", (req, res) => {
-  res.set("Content-Type", Prometheus.register.contentType);
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', Prometheus.register.contentType);
   res.end(Prometheus.register.metrics());
 });
 
@@ -67,12 +121,12 @@ app.use((req, res, next) => {
   next();
 });
 
-const server = app.listen("8080", () => {
+const server = app.listen('8080', () => {
   console.log(`Example app listening on port 8080!`);
 });
 
 // Graceful shutdown
-process.on("SIGTERM", () => {
+process.on('SIGTERM', () => {
   clearInterval(metricsInterval);
 
   server.close(err => {
